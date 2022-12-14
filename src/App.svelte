2c73c118383ab2f686d40247ec5e1b2f7eafc9f2
M.D. Walters <808426@pdsb.net>
2022-12-14 14:13:57 +0000

    Main is Pain

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,37 @@
 <script>
-// @ts-nocheck
+    // @ts-nocheck
 
-export let itemCount = 1;
+    export let itemCount = 1;
 
-document.addEventListener("keypress", (event) => {
-    if (event.key == "Enter") {
-        document.getElementById("add-item-button").click();
-    };
-});
-</script>
-
-<ul class="list-group m-3" id="items"></ul>
-<br>
-<br>
-
-<div class="input-group mb-3 fixed-bottom">
-    <input type="text" class="form-control ms-3" placeholder="Type a list item here" id="add-item-input" on:keydown={(event) => {
+    document.addEventListener("keypress", (event) => {
         if (event.key == "Enter") {
             document.getElementById("add-item-button").click();
         };
-    }}>
-    <button class="btn btn-primary input-group-text me-3" id="add-item-button" on:click={() => {
-        let li = document.createElement("li");
-        let input = document.getElementById("add-item-input");
-        let items = document.getElementById("items");
+    });
+</script>
 
-        li.classList.add("list-group-item");
-        li.id = `item-${itemCount}`;
-        li.innerHTML = input.value;
-        input.value = "";
-        items.appendChild(li);
-        itemCount++;
-    }}>Add</button>
-</div>
+<main>
+    <ul class="list-group m-3" id="items"></ul>
+    <br>
+    <br>
+
+    <div class="input-group mb-3 fixed-bottom">
+        <input type="text" class="form-control ms-3" placeholder="Type a list item here" id="add-item-input" on:keydown={(event) => {
+            if (event.key == "Enter") {
+                document.getElementById("add-item-button").click();
+            };
+        }}>
+        <button class="btn btn-primary input-group-text me-3" id="add-item-button" on:click={() => {
+            let li = document.createElement("li");
+            let input = document.getElementById("add-item-input");
+            let items = document.getElementById("items");
+
+            li.classList.add("list-group-item");
+            li.id = `item-${itemCount}`;
+            li.innerHTML = input.value;
+            input.value = "";
+            items.appendChild(li);
+            itemCount++;
+        }}>Add</button>
+    </div>
+</main>
