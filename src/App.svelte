fede0425db8178c28f397b05e4bdc706048e71b3
M.D. Walters <popularvsofficial@gmail.com>
2023-01-18 11:43:24 -0500

    We really needed this fix...

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <main>
-    <div id="items"></div>
+    <div class="list-group" id="items"></div>
     <br>
     <br>
 
@@ -23,7 +23,7 @@
             let input = document.getElementById("add-item-input");
             let item = document.getElementById("items");
 
-            div.classList.add("list-group", "list-group-item", "text-break", "m-3", "rounded");
+            div.classList.add("list-group-item", "text-break", "m-3", "rounded");
             div.innerHTML = `<span class="badge text-bg-${$itemStatus[1]}">${$itemStatus[0]}</span>
 ${input.value}
 <span class="btn-close float-end d-print-none" data-bs-toggle="modal" data-bs-target="#deleteItem">
