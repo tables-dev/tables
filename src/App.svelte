e8bb7eca1097418a7488f008b214f91fe24c95ba
M.D. Walters <popularvsofficial@gmail.com>
2023-01-18 11:11:13 -0500

    No more XSS!

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@
             let item = document.getElementById("items");
 
             div.classList.add("list-group", "list-group-item", "text-break", "m-3");
-            div.innerHTML = `<span class="badge text-bg-${$itemStatus[1]}">${$itemStatus[0]}</span>
+            div.value = `<span class="badge text-bg-${$itemStatus[1]}">${$itemStatus[0]}</span>
 ${input.value}
 <span class="btn-close float-end d-print-none" data-bs-toggle="modal" data-bs-target="#deleteItem">
     <div class="visually-hidden">Remove item</div>    
